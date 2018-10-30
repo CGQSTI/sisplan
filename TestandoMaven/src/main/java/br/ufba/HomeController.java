bf02fc46ade4eea86e0116fb15c126c0734db812
Fernando <fercez@ufba.br>
2018-10-30 16:25:09 -0300

    Teste

---
+++ TestandoMaven/src/main/java/br/ufba/HomeController.java
@@ -0,0 +1,12 @@
+package br.ufba;
+
+public class HomeController {
+
+	public static void main(String[] args) {
+		teste();
+	}
+	
+	public static void teste() {
+		System.out.println("teste");
+	}
+}
